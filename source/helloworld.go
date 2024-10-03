35c997e9bad5ca79353a664e1b9c4c50727dc61a
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2024-10-03 08:59:19 +0000

    add hello world in golang closes #28

---
+++ source/helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+/**
+ * main - a yet simple hello world in Golang
+ * @author: https://github.com/geoffrey - Geoffrey Logovi
+ */
+func main() {
+    fmt.Println("hello world")
+}
