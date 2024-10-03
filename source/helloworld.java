1c460d1f6d5d5fc151a3196252cf5b5fd3027933
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2024-10-03 07:40:40 +0000

    add helloworld in java closes Add “Hello world” for the Java programming language  #11

---
+++ source/helloworld.java
@@ -0,0 +1,13 @@
+/**
+ * HelloWorld - a java class that returns the string "hello world!"
+ * @author: https://github.com/geoffrey - Geoffrey Logovi
+ */
+
+class HelloWorld {
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
+
