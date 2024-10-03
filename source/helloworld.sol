1ccc7d170419ab9b8d081aa068607a88d7459276
starlabman <akodjolabore@gmail.com>
2024-10-03 11:42:28 +0000

    add Helloworld in bash language

--- source/helloworld.sol
+++ source/helloworld.sol
@@ -1,17 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Author: Laboré ( 0xWeb3DevRel)
-// username github: starlabman 
-
 contract HelloWorld {
-    string public message;
-
-    constructor() {
-        message = "Hello, World!";
-    }
-
-    function getMessage() public view returns (string memory) {
-        return message;
-    }
+    string public message = "Hello, World!";
 }
