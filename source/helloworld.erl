a7ec0ff88562409318a0cdabae3868a37b37bff0
Just2sire <le05onze2004@gmail.com>
2024-10-11 08:27:22 +0000

    Actualling setting up the info resume screen

--- source/helloworld.erl
+++ source/helloworld.erl
@@ -1,4 +1,7 @@
 -module(hello).
--export([hello_world/0]).
+-export([hello_world/0, main/1]).
 
-hello_world() -> io:fwrite("hello world").
+hello_world() -> io:fwrite("hello world\n").
+
+main(_Args) ->
+    hello_world().
