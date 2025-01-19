1a9b4736d0d9097f2896f6c426e598ed696dd102
Pratik <ghugepratik52@gmail.com>
2025-01-19 17:12:16 +0530

    Add Hello World example in LiveCode

--- script.js
+++ script.js
@@ -683,6 +683,11 @@ const languages = [
         name: 'COBOLScript',
         description: 'Scripting variant of COBOL',
         link: 'https://sites.google.com/view/cobolscripting'  
+    },
+    {
+        name: 'LiveCode',
+        description: 'Visual, high-level programming language',
+        link: 'https://livecode.com/'  
     }
 
 ];
