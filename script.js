09dfb554abb4f7a193d15d59026a98f938cbffc8
Avni Singh <avni.dns@gmail.com>
2025-02-18 15:47:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -788,6 +788,11 @@ const languages = [
         name: 'Rockstar',
         description: 'An esoteric programming language inspired by rock song lyrics, designed to turn code into poetic expressions',
         link: 'https://codewithrockstar.com/'
+    },
+    {
+        name: 'ZIMPL',
+        description: 'little language to translate the mathematical model of a problem into code',
+        link: 'https://zimpl.zib.de/'
     }
 
 ];
